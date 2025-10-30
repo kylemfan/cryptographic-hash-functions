8dd61757cce19fa93ba073daed86b4b49628980c
SpectreCobra500 <tcburkley@proton.me>
2025-10-30 12:50:09 -0700

    nltk not working

--- hash.py
+++ hash.py
@@ -1,4 +1,7 @@
 from Crypto.Hash import SHA256
+from pypdf import PdfReader
+import os
+#import nltk
 import bcrypt
 import secrets
 import string
@@ -13,7 +16,7 @@ def random_string(length=8) -> str:
   return ''.join(secrets.choice(chars) for i in range(length))
 
 # weak collision resistance
-def find_wcr() -> str:
+def find_wcr():
   m0 = b'example'
   digest1 = hash_input(m0)
   m1 = random_string(len(m0)).encode('utf-8')
@@ -25,7 +28,7 @@ def find_wcr() -> str:
   print(f'\n\tCOLLISION FOUND:\n m0: {m0} | digest: {digest1} \n m1: {m1} | digest: {digest2}\n')
 
 # strong collision resistance
-def find_scr() -> str:
+def find_scr():
   digests = {} # digest : message
   m0 = random_string(random.randint(1, 10)).encode('utf-8')
   digest1 = hash_input(m0)
@@ -40,5 +43,27 @@ def find_scr() -> str:
     else:
       digests[m1] = digest2
 
+# start breaking the hashes:
+# https://www.geeksforgeeks.org/python/working-with-pdf-files-in-python/
+def break_hash():
+  reader = PdfReader("shadow.pdf")
+  page = reader.pages[0]
+  print(page.extract_text())
+  page_data = page.extract_text().split('\n')
+  i = 0
+  while i < 16:
+    i += 1
+    if(os.fork() == 0):
+      print(f"fork {i} {os.getpid()}")
+      os._exit(0)
+  # wait for child processes
+  while 1:
+    try:
+      pid, status = os.wait()
+    except ChildProcessError:
+      break
+
+
 if __name__ == '__main__':
   find_scr()
+  print(break_hash())
